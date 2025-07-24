72f8bece6644ce002fcccc192c018bb9206702e4
YudhiPrasetya <ydh.pr453ty0@gmail.com>
2025-07-24 11:27:08 +0700

    Update tamplikan_sewing_monitoring.php
    Unremarked qc_endline variable

--- tamplikan_sewing_monitoring.php
+++ tamplikan_sewing_monitoring.php
@@ -498,7 +498,7 @@
 
          var arrEffPros = [];
 
-         // var qc_endline = new WebSocket("ws://192.168.90.100:10000/?service=qc_endline");
+         var qc_endline = new WebSocket("ws://192.168.90.100:10000/?service=qc_endline");
          // var qc_endline = new WebSocket("ws://localhost:10000/?service=qc_endline");
 
          function LoadDataEffQCEndline(dataArr){
