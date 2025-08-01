3eb6ddda083dd0df8f9006ddd0fc1dd4c6957190
YudhiPrasetya <ydh.pr453ty0@gmail.com>
2025-08-01 10:20:55 +0700

    Update edit_master_order.php
    change label preparation to juwita

--- edit_master_order.php
+++ edit_master_order.php
@@ -211,7 +211,8 @@ require_once 'view/header.php';
           <!-- <label class="form-check-label" style="font-weight: normal" for="inlineCheckbox<//?= $i; ?>"><//?= strtoupper(($data2['nama_transaksi'] == "press" ? "washing" : $data2['nama_transaksi'])) ?></label> -->
 
           <label class="form-check-label" style="font-weight: normal" for="inlineCheckbox<?= $i; ?>"><?= strtoupper(
-            ($data2['nama_transaksi'] == "washing" ? "f qc" : ($data2['nama_transaksi'] == "qc_buyer" ? "furushima" :($data2['nama_transaksi'] == "press" ? "padprint" : ($data2['nama_transaksi'] == "bemis" ? "fuse" : $data2['nama_transaksi']))))
+            ($data2['nama_transaksi'] == "washing" ? "f qc" : ($data2['nama_transaksi'] == "qc_buyer" ? "furushima" :($data2['nama_transaksi'] == "press" ? "padprint" : ($data2['nama_transaksi'] == "bemis" ? "fuse" :
+            ($data2['nama_transaksi'] == "preparation" ? "juwita" : $data2['nama_transaksi'])))))
             ) ?></label>
         </td>
         <?php }else{ ?>
